5351506c455f41033ec54f9caecd6711a37c1898
unknown <cogdotnet515@iiht.tech>
2022-07-07 13:17:21 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddCompanyComponent } from './add-company/add-company.component';
+import { AddStockComponent } from './add-stock/add-stock.component';
+import { StockGridComponent } from './stock-grid/stock-grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'addcompany', component: AddCompanyComponent },
+  { path: 'addstock', component: AddStockComponent },
+  { path: 'stocks', component: StockGridComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddCompanyComponent } from './add-company/add-company.component';
+import { AddStockComponent } from './add-stock/add-stock.component';
+import { StockGridComponent } from './stock-grid/stock-grid.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CompanyService } from './Services/company.service';
+import { StockService } from './Services/stock.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddCompanyComponent,
+    AddStockComponent,
+    StockGridComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
